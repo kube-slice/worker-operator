a207ff265b64b3f06343a5cb65d02b6652d68251
Md Suman Sarker <suman@appscode.com>
2026-01-05 10:49:30 +0600

    update vl3 image
    Signed-off-by: Md Suman Sarker <suman@appscode.com>

--- controllers/slice/slicerouter.go
+++ controllers/slice/slicerouter.go
@@ -97,7 +97,6 @@ func (r *SliceReconciler) getContainerSpecForSliceRouter(s *kubeslicev1beta1.Sli
 	vl3ImagePullPolicy := corev1.PullAlways
 
 	vl3Image := os.Getenv("AVESHA_VL3_ROUTER_IMAGE")
-	vl3Image = "sumon124816/kubeslice:vl3-spire"
 	vl3RouterPullPolicy := os.Getenv("AVESHA_VL3_ROUTER_PULLPOLICY")
 
 	if len(vl3RouterPullPolicy) != 0 {
